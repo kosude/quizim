5db73d30a7eeddcbd1f6923e4b927b67ce8fcb8f
Jack Bennett <jackbennett0708@gmail.com>
2024-03-24 20:50:09 +0000

    Set up template ASP.NET web app project + cert trust script

---
+++ scripts/trust-cert.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+###################################################################################
+#                                                                                 #
+# From https://forum.manjaro.org/t/ssl-cerificates-for-local-development/139354/5 #
+#                                                                                 #
+###################################################################################
+
+set -e
+
+cd $HOME || exit
+
+# e.g. aspnet dev cert is located at /usr/local/share/ca-certificates/aspnet/
+CERT_HOME="/usr/local/share/ca-certificates"
+
+dotnet dev-certs https
+sudo -E dotnet dev-certs https -ep localhost.crt --format Pem
+
+# Setup Firefox
+echo "{
+    \"policies\": {
+        \"Certificates\": {
+            \"Install\": [
+            	\"aspnetcore-localhost-https.crt\"
+            ]
+        }
+    }
+}" > policies.json
+
+# Trust Firefox
+sudo mv policies.json /usr/lib/firefox/distribution/
+mkdir -p ~/.mozilla/certificates
+sudo cp localhost.crt ~/.mozilla/certificates/aspnetcore-localhost-https.crt
+
+# Trust Chromium based browsers
+sudo certutil -d sql:$HOME/.pki/nssdb -A -t "P,," -n localhost -i ./localhost.crt
+sudo certutil -d sql:$HOME/.pki/nssdb -A -t "C,," -n localhost -i ./localhost.crt
+
+sudo trust anchor --store localhost.crt
+
+# # Trust wget
+# sudo cp localhost.crt $CERT_HOME/aspnet/https.crt
+# sudo cp localhost.crt $CERT_HOME/trust-source/anchors/aspnetcore-https-localhost.pem
+# sudo cp localhost.crt $CERT_HOME/aspnet/aspnetcore-https-localhost.pem
+# sudo chmod 666 $CERT_HOME/trust-source/anchors/aspnetcore-https-localhost.pem
+# sudo chmod 666 $CERT_HOME/aspnet/aspnetcore-https-localhost.pem
+
+# Trust dotnet-to-dotnet
+sudo cp localhost.crt /etc/ssl/certs/aspnetcore-https-localhost.pem
+
+# Clean up
+sudo update-ca-trust
+sudo update-ca-trust extract
+rm -f localhost.crt
